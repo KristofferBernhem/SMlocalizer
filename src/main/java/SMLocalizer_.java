56bfe9790524d80eb9c1e581a40a246378a46ca0
Kristoffer Bernhem <kristoffer.bernhem@scilifelab.se>
2016-07-05 12:03:37 +0200

    Renamed

--- src/main/java/SMLocalizer_.java
+++ src/main/java/SMLocalizer_.java
@@ -5,13 +5,13 @@ import ij.plugin.PlugIn;
 import net.imagej.ImageJ;
 
 
-public class Evaluate_ implements PlugIn {
+public class SMLocalizer_ implements PlugIn {
 
 	public static void main(final String... args) throws Exception {
 		// create the ImageJ application context with all available services
 		final ImageJ ij = new ImageJ();
 		ij.ui().showUI();
-		Class<?> clazz = Evaluate_.class;
+		Class<?> clazz = SMLocalizer_.class;
 		String url = clazz.getResource("/" + clazz.getName().replace('.', '/') + ".class").toString();
 		String pluginsDir = url.substring("file:".length(), url.length() - clazz.getName().length() - ".class".length());
 		System.setProperty("plugins.dir", pluginsDir);
