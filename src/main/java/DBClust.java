024054ab882fdb0201e1f93322a8e2076dc74a8d
Kristoffer Bernhem <kristoffer.bernhem@scilifelab.se>
2016-07-06 11:17:13 +0200

    Bug fixes, removed batch, functional parameter plot.

--- src/main/java/DBClust.java
+++ src/main/java/DBClust.java
@@ -12,9 +12,9 @@ import ij.process.ByteProcessor;
 
 /*
  * Change to update table with cluster information.
+ * Create version for 3D data.
  */
 public class DBClust {
-
 	public static List<Cluster<DoublePoint>> Ident(double eps, int minPts, int pixelSize){
 		ArrayList<Particle> InpParticle = TableIO.Load(); // Get current table data.
 		List<DoublePoint> points = new ArrayList<DoublePoint>();
@@ -29,8 +29,8 @@ public class DBClust {
 		}
 		DBSCANClusterer<DoublePoint> DB = new DBSCANClusterer<DoublePoint>(eps, minPts);
 		List<Cluster<DoublePoint>> cluster = DB.cluster(points);
-		int ClustIdx = 1;
-		int[] IndexList = new int[InpParticle.size()];
+		int ClustIdx 			= 1;
+		int[] IndexList 		= new int[InpParticle.size()];
 		int Width 				= 0;
 		int Height 				= 0;
 		int Pad					= 100;
@@ -56,49 +56,47 @@ public class DBClust {
 		}  
 		Width = Math.round(Width/pixelSize);
 		Height = Math.round(Height/pixelSize);
-		ByteProcessor IP  = new ByteProcessor(Width,Height);			
-		for (int x = 0; x < Width; x++){
-			for (int y = 0; y < Height; y++){
-				IP.putPixel(x, y, 0); // Set all data points to 0 as start.
-			}
-			
-		}
-		ij.measure.ResultsTable tab = Analyzer.getResultsTable();
-		tab.reset();		
-		for (int i = 0; i < InpParticle.size(); i++){
-			tab.incrementCounter();
-			tab.addValue("Cluster", IndexList[i]);
-			tab.addValue("x0", InpParticle.get(i).x);
-			tab.addValue("y0", InpParticle.get(i).y);
-			tab.addValue("z0", InpParticle.get(i).z);
-			tab.addValue("frame", InpParticle.get(i).frame);
-			tab.addValue("channel", InpParticle.get(i).channel);
-			tab.addValue("sigma_x", InpParticle.get(i).sigma_x);
-			tab.addValue("sigma_y", InpParticle.get(i).sigma_y);
-			tab.addValue("sigma_z", InpParticle.get(i).sigma_z);
-			tab.addValue("precision_x", InpParticle.get(i).precision_x);
-			tab.addValue("precision_y", InpParticle.get(i).precision_y);
-			tab.addValue("precision_z", InpParticle.get(i).precision_z);
-			tab.addValue("chi_square", InpParticle.get(i).chi_square);
-			tab.addValue("photons", InpParticle.get(i).photons);
-			if (IndexList[i] == 0){
-				tab.addValue("include", 0);
-			}else{
-				tab.addValue("include", 1);
-				int x = (int) Math.round(InpParticle.get(i).x/pixelSize);
-				int y = (int) Math.round(InpParticle.get(i).y/pixelSize);		
-				IP.putPixel(x, y, 1);
-			}
-			
-		}
-		tab.show("Results");
+		if(Width > 0 && Height > 0){
 
+			ByteProcessor IP  = new ByteProcessor(Width,Height);			
+			for (int x = 0; x < Width; x++){
+				for (int y = 0; y < Height; y++){
+					IP.putPixel(x, y, 0); // Set all data points to 0 as start.
+				}
 
-	
-		ImagePlus Image = new ImagePlus("ClusterData",IP);
-		Image.setImage(Image);
-		Image.show(); 														// Make visible
-		
+			}
+			ij.measure.ResultsTable tab = Analyzer.getResultsTable();
+			tab.reset();		
+			for (int i = 0; i < InpParticle.size(); i++){
+				tab.incrementCounter();
+				tab.addValue("Cluster", IndexList[i]);
+				tab.addValue("x0", InpParticle.get(i).x);
+				tab.addValue("y0", InpParticle.get(i).y);
+				tab.addValue("z0", InpParticle.get(i).z);
+				tab.addValue("frame", InpParticle.get(i).frame);
+				tab.addValue("channel", InpParticle.get(i).channel);
+				tab.addValue("sigma_x", InpParticle.get(i).sigma_x);
+				tab.addValue("sigma_y", InpParticle.get(i).sigma_y);
+				tab.addValue("sigma_z", InpParticle.get(i).sigma_z);
+				tab.addValue("precision_x", InpParticle.get(i).precision_x);
+				tab.addValue("precision_y", InpParticle.get(i).precision_y);
+				tab.addValue("precision_z", InpParticle.get(i).precision_z);
+				tab.addValue("chi_square", InpParticle.get(i).chi_square);
+				tab.addValue("photons", InpParticle.get(i).photons);
+				if (IndexList[i] == 0){
+					tab.addValue("include", 0);
+				}else{
+					tab.addValue("include", 1);
+					int x = (int) Math.round(InpParticle.get(i).x/pixelSize);
+					int y = (int) Math.round(InpParticle.get(i).y/pixelSize);		
+					IP.putPixel(x, y, 1);
+				}
+			}
+			tab.show("Results");
+			ImagePlus Image = new ImagePlus("ClusterData",IP);
+			Image.setImage(Image);
+			Image.show(); 														// Make visible
+		}
 		return cluster; 
 	}
 	public static int getIdx(double x, double y, int width, int height){
--- src/main/java/parameterDistribution.java
+++ src/main/java/parameterDistribution.java
@@ -1,14 +1,11 @@
 import java.awt.Color;
 import java.util.ArrayList;
-
-import ij.IJ;
-import net.imagej.ImageJ;
 import ij.gui.Plot;
-import ij.plugin.PlugIn;
 
-public class parameterDistribution implements PlugIn{
 
-	public static void main(String[] args){
+public class parameterDistribution{
+
+/*	public static void main(String[] args){
 		final ImageJ ij = new ImageJ();
 		ij.ui().showUI();
 
@@ -19,81 +16,137 @@ public class parameterDistribution implements PlugIn{
 		String url = clazz.getResource("/" + clazz.getName().replace('.', '/') + ".class").toString();
 		String pluginsDir = url.substring("file:".length(), url.length() - clazz.getName().length() - ".class".length());
 		System.setProperty("plugins.dir", pluginsDir);
-		IJ.runPlugIn(clazz.getName(), "");
-	}
 
-
-	
-	public void run(String arg0) {
-		int nBins = 50;
-		
 		/*
 		 * testdata:
 		 */
-		int nFrame =100;
+/*		int nFrame =100;
 		int width = 64;
 		int height = 64;
 		int perFrame = 10;
 		int total = 100;
 		ArrayList<Particle> ParticleList = TestData.generate(nFrame, width, height, perFrame, total);
-		
-		/*
-		 * write for all parameters of interest. Multiwindow plotting? two color for xy
-		 */
-		
-		double[] SigmaX = new double[ParticleList.size()];
-		double[] SigmaY = new double[ParticleList.size()];
-		double[] PrecisionX = new double[ParticleList.size()];
-		double[] PrecisionY = new double[ParticleList.size()];
-		double[] CbiSquare = new double[ParticleList.size()];
-		double[] Photons = new double[ParticleList.size()];
-		for (int i = 0; i < SigmaX.length; i++){
-			SigmaX[i] = ParticleList.get(i).sigma_x;
-			SigmaY[i] = ParticleList.get(i).sigma_y;
-			PrecisionX[i] = ParticleList.get(i).precision_x;
-			PrecisionY[i] = ParticleList.get(i).precision_y;
-			CbiSquare[i] = ParticleList.get(i).chi_square;
-			Photons[i] = ParticleList.get(i).photons;
-			
+		TableIO.Store(ParticleList);
+		IJ.runPlugIn(clazz.getName(), "");
+	} */
+
+
+
+	public static void run() {
+		int nBins = 100;
+		ArrayList<Particle> ParticleList = TableIO.Load(); // Get data.
+
+
+		int Channel = 1;
+		for (int i = 0; i < ParticleList.size(); i++){
+			if ( ParticleList.get(i).channel > Channel){
+				Channel = (int) ParticleList.get(i).channel;
+			}
 		}
-		BackgroundCorrection.quickSort(SigmaX, 0, SigmaX.length-1);
-		BackgroundCorrection.quickSort(SigmaY, 0, SigmaY.length-1);
-		BackgroundCorrection.quickSort(PrecisionX, 0, SigmaY.length-1);
-		BackgroundCorrection.quickSort(PrecisionY, 0, SigmaY.length-1);
-		BackgroundCorrection.quickSort(CbiSquare, 0, SigmaY.length-1);
-		BackgroundCorrection.quickSort(Photons, 0, SigmaY.length-1);
-		double SigmaXmin = SigmaX[0]/1.1;
-		double SigmaXmax = SigmaX[SigmaX.length-1]*1.1;		
-		double[] x_axis = correctDrift.interp(SigmaXmin, SigmaXmax, nBins);
-		double[] binnedDataX = binData(SigmaX,nBins,x_axis);
-		double[] binnedDataY = binData(SigmaY,nBins,x_axis);
-		String[] headerSigma = {"Sigma", "sigma [nm]", "count"};
-		plot(headerSigma,"Sigma_x \n Sigma_y",binnedDataX,binnedDataY,x_axis);
-		
-		
-		double PrecisionMin = PrecisionX[0]/1.1;
-		double PrecisionMax = PrecisionX[PrecisionX.length-1]*1.1;
-		x_axis = correctDrift.interp(PrecisionMin, PrecisionMax, nBins);
-		String[] headerPrecision = {"Precision", "precision [nm]", "count"};
-		plot(headerPrecision,"Precision_x \n Precision_y",binData(PrecisionX,nBins,x_axis),binData(PrecisionY,nBins,x_axis),correctDrift.interp(PrecisionMin, PrecisionMax, nBins));
-		
-		
-		double ChiSquareMin = CbiSquare[0]/1.1;
-		double ChiSquareMax = CbiSquare[CbiSquare.length-1]*1.1;
-		x_axis = correctDrift.interp(ChiSquareMin, ChiSquareMax, nBins);
-		String[] headerChiSquare = {"Chi^2", "Chi^2", "count"};
-		plot(headerChiSquare,"Chi^2",binData(CbiSquare,nBins,x_axis),correctDrift.interp(ChiSquareMin, ChiSquareMax, nBins));			
-		
-		double PhotonsMin = Photons[0]/1.1;
-		double PhotonsMax = Photons[Photons.length-1]*1.1;
-		x_axis = correctDrift.interp(PhotonsMin, PhotonsMax, nBins);
-		String[] headerSPhotons = {"Photons", "photons", "count"};
-		plot(headerSPhotons,"Photons",binData(Photons,nBins,x_axis),correctDrift.interp(PhotonsMin, PhotonsMax, nBins));
+		for (int Ch = 1; Ch <= Channel; Ch++){ // Remake based on logic from drift correction binning.
+			double[] tempSigmaX = new double[ParticleList.size()];
+			double[] tempSigmaY = new double[ParticleList.size()];
+			double[] tempSigmaZ = new double[ParticleList.size()];
+			double[] tempPrecisionX = new double[ParticleList.size()];
+			double[] tempPrecisionY = new double[ParticleList.size()];
+			double[] tempPrecisionZ = new double[ParticleList.size()];
+			double[] tempCbiSquare = new double[ParticleList.size()];
+			double[] tempPhotons = new double[ParticleList.size()];
+			int count = 0;
+			for (int i = 0; i < ParticleList.size(); i++){
+				if(ParticleList.get(i).channel == Ch){
+					tempSigmaX[i] 		= ParticleList.get(i).sigma_x;
+					tempSigmaY[i] 		= ParticleList.get(i).sigma_y;
+					tempSigmaZ[i] 		= ParticleList.get(i).sigma_z;
+					tempPrecisionX[i] 	= ParticleList.get(i).precision_x;
+					tempPrecisionY[i] 	= ParticleList.get(i).precision_y;
+					tempPrecisionZ[i] 	= ParticleList.get(i).precision_z;
+					tempCbiSquare[i] 	= ParticleList.get(i).chi_square;
+					tempPhotons[i] 		= ParticleList.get(i).photons;
+					count++;
+				}
+			}
+			
+			double[] SigmaX = new double[count];
+			double[] SigmaY = new double[count];
+			double[] SigmaZ = new double[count];
+			double[] PrecisionX = new double[count];
+			double[] PrecisionY = new double[count];
+			double[] PrecisionZ = new double[count];
+			double[] CbiSquare = new double[count];
+			double[] Photons = new double[count];
+			for(int i = 0; i < count; i++){
+				SigmaX[i] 		= tempSigmaX[i];
+				SigmaY[i] 		= tempSigmaY[i];
+				SigmaZ[i] 		= tempSigmaZ[i];
+				PrecisionX[i] 	= tempPrecisionX[i];
+				PrecisionY[i] 	= tempPrecisionY[i];
+				PrecisionZ[i] 	= tempPrecisionZ[i];
+				CbiSquare[i] 	= tempCbiSquare[i];
+				Photons[i] 		= tempPhotons[i];
+			}
+			
+			BackgroundCorrection.quickSort(SigmaX, 0, SigmaX.length-1);
+			BackgroundCorrection.quickSort(SigmaY, 0, SigmaY.length-1);
+			BackgroundCorrection.quickSort(SigmaZ, 0, SigmaZ.length-1);
+			BackgroundCorrection.quickSort(PrecisionX, 0, PrecisionX.length-1);
+			BackgroundCorrection.quickSort(PrecisionY, 0, PrecisionY.length-1);
+			BackgroundCorrection.quickSort(PrecisionZ, 0, PrecisionZ.length-1);
+			BackgroundCorrection.quickSort(CbiSquare, 0, CbiSquare.length-1);
+			BackgroundCorrection.quickSort(Photons, 0, Photons.length-1);
+			double SigmaMin = SigmaX[0]/1.1;
+			double SigmaMax = SigmaX[SigmaX.length-1]*1.1;
+			if (SigmaY[0]/1.1 < SigmaMin)
+				SigmaMin = SigmaY[0]/1.1;
+			if (SigmaY[SigmaY.length-1]*1.1 > SigmaMax)
+				SigmaMax = SigmaY[SigmaY.length-1]*1.1;
+
+			double[] x_axis = correctDrift.interp(SigmaMin, SigmaMax, nBins);
+			double[] binnedDataX = binData(SigmaX,nBins,x_axis);
+			double[] binnedDataY = binData(SigmaY,nBins,x_axis);
+			String[] headerSigma = {("Channel " + Ch +" sigma"), "sigma [nm]", "count"};
+			plot(headerSigma,"Sigma_x \n Sigma_y",binnedDataX,binnedDataY,x_axis);
+			
+			double SigmaZmin = SigmaZ[0]/1.1;
+			double SigmaZmax = SigmaZ[SigmaZ.length-1]*1.1;
+			x_axis = correctDrift.interp(SigmaZmin, SigmaZmax, nBins);
+			String[] headerSigmaZ = {("Channel " + Ch +" sigma z"), "sigma z [nm]", "count"};
+			plot(headerSigmaZ,"Sigma_z",binData(SigmaZ,nBins,x_axis),correctDrift.interp(SigmaZmin, SigmaZmax, nBins));			
+
+			double PrecisionMin = PrecisionX[0]/1.1;
+			double PrecisionMax = PrecisionX[PrecisionX.length-1]*1.1;
+			if (PrecisionY[0]/1.1 < PrecisionMin)
+				PrecisionMin = PrecisionY[0]/1.1;
+			if (PrecisionY[PrecisionY.length-1]*1.1 > PrecisionMax)
+				PrecisionMax = PrecisionY[PrecisionY.length-1]*1.1;
+
+			x_axis = correctDrift.interp(PrecisionMin, PrecisionMax, nBins);
+			String[] headerPrecision = {("Channel " + Ch +" Precision"), "precision [nm]", "count"};
+			plot(headerPrecision,"Precision_x \n Precision_y",binData(PrecisionX,nBins,x_axis),binData(PrecisionY,nBins,x_axis),correctDrift.interp(PrecisionMin, PrecisionMax, nBins));		
+			
+			double PrecisionZmin = PrecisionZ[0]/1.1;
+			double PrecisionZmax = PrecisionZ[PrecisionZ.length-1]*1.1;
+			x_axis = correctDrift.interp(PrecisionZmin, PrecisionZmax, nBins);
+			String[] headerPrecisionZ = {("Channel " + Ch +" Chi^2"), "Chi^2", "count"};
+			plot(headerPrecisionZ,"Precision_z",binData(PrecisionZ,nBins,x_axis),correctDrift.interp(PrecisionZmin, PrecisionZmax, nBins));			
+
+			double ChiSquareMin = CbiSquare[0]/1.1;
+			double ChiSquareMax = CbiSquare[CbiSquare.length-1]*1.1;
+			x_axis = correctDrift.interp(ChiSquareMin, ChiSquareMax, nBins);
+			String[] headerChiSquare = {("Channel " + Ch +" Chi^2"), "Chi^2", "count"};
+			plot(headerChiSquare,"Chi^2",binData(CbiSquare,nBins,x_axis),correctDrift.interp(ChiSquareMin, ChiSquareMax, nBins));			
+
+			double PhotonsMin = Photons[0]/1.1;
+			double PhotonsMax = Photons[Photons.length-1]*1.1;
+			x_axis = correctDrift.interp(PhotonsMin, PhotonsMax, nBins);
+			String[] headerSPhotons = {("Channel " + Ch +" 	aPhotons"), "photons", "count"};
+			plot(headerSPhotons,"Photons",binData(Photons,nBins,x_axis),correctDrift.interp(PhotonsMin, PhotonsMax, nBins));
+		} // End loop over channels.
 	}
-	
+
 	static double[] binData(double[] sortedData, int nBins,double[] x_axis){
 		double[] binnedData = new double[nBins];
-				
+
 		int currBin = 1;
 		for (int i = 0; i < sortedData.length; i++){
 			if (sortedData[i] <= x_axis[currBin-1]){
@@ -104,16 +157,14 @@ public class parameterDistribution implements PlugIn{
 			}							 
 		}
 		return binnedData;
-		
 	}
-	
+
 	static void plot(String[] header, String Legend, double[] values,double[] x_axis) {
-		//double[] x = new double[values.length];
 		Plot newPlot = new Plot(header[0],header[1],header[2]);
 		newPlot.addPoints(x_axis,values, Plot.LINE);
 		newPlot.addLegend(Legend);
 		newPlot.show();
-		
+
 	}
 	static void plot(String[] header, String Legend, double[] values, double[] values2,double[] x_axis) {
 		Plot newPlot = new Plot(header[0],header[1],header[2]);
